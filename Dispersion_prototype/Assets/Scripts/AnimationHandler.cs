3d042dccceb559336e14152bb4215f2d0f8c6af8
Kis Lázár Bence <akislb99@gmail.com>
2020-05-04 18:22:15 +0200

    Generalized interactions

---
+++ Dispersion_prototype/Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class AnimationHandler : MonoBehaviour
+{
+    private Animation anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    public void Play()
+    {
+        anim.Play();
+    }
+}
---
+++ Dispersion_prototype/Assets/Scripts/Managers/Interactions/Interaction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum EventType { Use }
+public class Interaction : MonoBehaviour
+{
+    public EventType eventType;
+    public UnityEvent action;
+
+    void Awake()
+    {
+        if (action == null)
+            action = new UnityEvent();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (eventType == EventType.Use && (other.tag == "Player" || other.tag == "Clone") && Input.GetButton("Use"))
+        {
+            action.Invoke();
+        }
+    }
+}
