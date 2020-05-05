81217f6b812ab2d16e598cccbddb42523083cfef
Kis Lázár Bence <akislb99@gmail.com>
2020-05-05 15:46:46 +0200

    Doors open for a limited time + other animation functions
    • play
    • play reverse
    • play, wait, reverse
    • toggle play the opposite of previously played animation

--- Dispersion_prototype/Assets/Scripts/AnimationHandler.cs
+++ Dispersion_prototype/Assets/Scripts/AnimationHandler.cs
@@ -5,14 +5,71 @@ using UnityEngine;
 public class AnimationHandler : MonoBehaviour
 {
     private Animation anim;
+    private string clipName;
+    private string reverseName;
+    private bool crRunning = false;
+    private bool isReversed = false;
 
     private void Awake()
     {
         anim = GetComponentInChildren<Animation>();
+        clipName = anim.clip.name;
+        reverseName = clipName + "_reverse";
     }
 
     public void Play()
     {
         anim.Play();
+    }
+
+    public void PlayReverse()
+    {
+        anim.Play(reverseName);
+    }
+
+    public void PlayToggle()
+    {
+        if (!anim.isPlaying)
+        {
+            if (isReversed)
+            {
+                PlayReverse();
+                isReversed = false;
+            }
+            else
+            {
+                Play();
+                isReversed = true;
+            }
+        }
+    }
+
+    public void PlayAndWaitBeforeReverse(float seconds)
+    {
+        if (!crRunning)
+        {
+            StartCoroutine("PlayAndWait", seconds);
+        }
+    }
+
+    private IEnumerator PlayAndWait(float seconds)
+    {
+        crRunning = true;
+
+        Play();
+        while (anim.isPlaying)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(seconds);
+
+        PlayReverse();
+        while (anim.isPlaying)
+        {
+            yield return null;
+        }
+
+        crRunning = false;
     }
 }
