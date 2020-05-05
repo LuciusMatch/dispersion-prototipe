8217b332ae6e8eacd1a2d3797289cc25a7b71437
Kis Lázár Bence <akislb99@gmail.com>
2020-05-05 15:17:43 +0200

    Keycard indicator

--- Dispersion_prototype/Assets/Scripts/Managers/KeycardInventory.cs
+++ Dispersion_prototype/Assets/Scripts/Managers/KeycardInventory.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class KeycardInventory : MonoBehaviour
 {
+    public List<GameObject> indicators = new List<GameObject>();
+
     private List<int> keycards = new List<int>();
+
+    private void Start()
+    {
+        foreach (GameObject indicator in indicators)
+        {
+            indicator.SetActive(false);
+        }
+    }
 
     public bool HasKeycard(int id)
     {
@@ -14,6 +24,7 @@ public class KeycardInventory : MonoBehaviour
     public void AddKeycard(int id)
     {
         keycards.Add(id);
+        indicators[id].SetActive(true);
         Debug.Log("Keycard " + id + " collected");
     }
 }
