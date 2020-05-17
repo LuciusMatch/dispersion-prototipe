41819719d506c27becfcc7bc7876e61ea3fc6435
Kis Lázár Bence <akislb99@gmail.com>
2020-05-17 19:29:14 +0200

    Camera system cleanup

--- Dispersion_prototype/Assets/Scripts/Managers/Camera Scripts/CameraTrigger.cs
+++ Dispersion_prototype/Assets/Scripts/Managers/Camera Scripts/CameraTrigger.cs
@@ -1,40 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class CameraTrigger : MonoBehaviour
 {
     Camera cam;
     CameraController cameraController;
-    [SerializeField]
     Transform cameraTransform;
-    [SerializeField]
-    int cameraSize;
-    [SerializeField]
-    bool isOrtographic;
-    [SerializeField]
-    float cameraSpeed;
-    [SerializeField]
-    bool perspectiveFollow;
 
-    // Start is called before the first frame update
+    [SerializeField] int cameraSize;
+    [SerializeField] bool isOrtographic;
+    [SerializeField] float cameraSpeed;
+    [SerializeField] bool perspectiveFollow;
+
     void Start()
     {
         cam = GameManager.Instance.mainCamera.GetComponent<Camera>();
         cameraController = GameManager.Instance.mainCamera.GetComponent<CameraController>();
+
+        Assert.IsTrue(gameObject.GetComponentsInChildren<Transform>().Length > 1); // there is a child
+        cameraTransform = GetComponentsInChildren<Transform>()[1];
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-
             if (perspectiveFollow)
             {
                 cameraController.SetCameraFollowPlayer(false);
             }
             else
+            {
                 cameraController.SetNewCameraTransform(cameraTransform, cameraSize, cameraSpeed, isOrtographic);
+            }
         }
 
     }
