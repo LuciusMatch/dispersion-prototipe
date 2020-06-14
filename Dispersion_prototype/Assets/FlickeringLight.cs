e96aaac18f3020bdf6956ed44162ec85ad8e286a
LuciusMatch <stove1995@gmail.com>
2020-06-14 23:57:22 +0200

    Visuals 1
    Not complete, in case I will loose all the progress again

---
+++ Dispersion_prototype/Assets/FlickeringLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+
+    // Update is called once per frame
+    float minFlickerSpeed = 0.1f;
+    float maxFlickerSpeed = 1.0f;
+ 
+    void Update()
+    {
+        //light.enabled = true;
+        //yield WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed);
+        //light.enabled = false;
+        //yield WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed);
+    }
+}
