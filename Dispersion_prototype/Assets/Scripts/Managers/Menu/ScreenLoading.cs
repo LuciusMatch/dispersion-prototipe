ade9837e9b1ca34ddb708bc1ba26fc470ed1978f
LuciusMatch <stove1995@gmail.com>
2020-06-18 01:44:18 +0200

    Added loading screen

---
+++ Dispersion_prototype/Assets/Scripts/Managers/Menu/ScreenLoading.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScreenLoading : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    // Update is called once per frame
+    IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gamelevel = SceneManager.LoadSceneAsync(2);
+        while (gamelevel.progress < 1)
+        {
+            progressbar.fillAmount = gamelevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
